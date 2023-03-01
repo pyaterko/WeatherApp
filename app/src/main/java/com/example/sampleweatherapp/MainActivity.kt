a9f843f852ceaa51f459c518cd01e3a5addbb7d0
PC <pyaterko68@gmail.com>
2023-03-01 10:32:40 +0300

    activity_main

--- app/src/main/java/com/example/sampleweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleweatherapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.sampleweatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.elveum.elementadapter.simpleAdapter
+import com.example.sampleweatherapp.databinding.ActivityMainBinding
+import com.example.sampleweatherapp.databinding.ItemHorizontalBinding
+import com.example.sampleweatherapp.databinding.ItemVerticalBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
